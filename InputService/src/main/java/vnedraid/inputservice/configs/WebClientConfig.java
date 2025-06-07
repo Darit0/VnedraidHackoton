a5dec2c7fa17a7e21cf2e5a56c887f5203643032
LaaLkA <n.afonchenkov@yandex.ru>
2025-06-07 19:48:18 +0300

    [ADD] Input Service

---
+++ InputService/src/main/java/vnedraid/inputservice/configs/WebClientConfig.java
@@ -0,0 +1,28 @@
+package vnedraid.inputservice.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient hhWebClient(WebClient.Builder builder, ObjectMapper objectMapper) {
+        return builder
+                .baseUrl("https://api.hh.ru")
+                .defaultHeader(HttpHeaders.USER_AGENT, "Vnedraid-InputService/1.0")
+                .codecs(clientCodecConfigurer -> {
+                    clientCodecConfigurer.defaultCodecs()
+                            .jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+                    clientCodecConfigurer.defaultCodecs()
+                            .jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+                    // НЕ НАДО вручную подсовывать декодер gzip!
+                })
+                .build();
+    }
+}

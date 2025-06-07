c924bb3f4e9736da25b067739c05a16a3feeb0dd
LaaLkA <n.afonchenkov@yandex.ru>
2025-06-07 12:41:19 +0300

    [ADD] InputService

---
+++ InputService/src/main/java/vnedraid/inputservice/InputServiceApplication.java
@@ -0,0 +1,13 @@
+package vnedraid.inputservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InputServiceApplication.class, args);
+    }
+
+}
---
+++ InputService/src/test/java/vnedraid/inputservice/InputServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vnedraid.inputservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InputServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

724473f63330468c7e0f889cd0c83e696cce7618
LaaLkA <n.afonchenkov@yandex.ru>
2025-06-07 18:32:54 +0300

    [ADD] Input Service

--- InputService/src/main/java/vnedraid/inputservice/configs/HhWebConfig.java
+++ InputService/src/main/java/vnedraid/inputservice/configs/HhWebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import vnedraid.inputservice.services.impl.CollectorHH;
@@ -17,19 +18,23 @@ import java.time.Duration;
 public class HhWebConfig {
 
     @Bean
-    public WebClient hhWebClient(ObjectMapper mapper, WebClient.Builder builder){
+    public WebClient hhWebClient(ObjectMapper mapper, WebClient.Builder builder) {
+
+        Jackson2JsonDecoder decoder = new Jackson2JsonDecoder(mapper);
+        Jackson2JsonEncoder encoder = new Jackson2JsonEncoder(mapper);
+
         return builder
                 .baseUrl("https://api.hh.ru")
-                .defaultHeader(HttpHeaders.USER_AGENT,"Vnedraid-InputService/1.0")
-                .defaultHeader(HttpHeaders.ACCEPT_ENCODING,"gzip, deflate")
+                .defaultHeader(HttpHeaders.USER_AGENT, "Vnedraid-InputService/1.0")
+                .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate")
                 .clientConnector(new ReactorClientHttpConnector(
                         HttpClient.create()
                                 .responseTimeout(Duration.ofSeconds(25))
-                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,6000)))
+                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 6000)))
                 .codecs(c -> {
-                    c.defaultCodecs()
-                            .jackson2JsonDecoder(new Jackson2JsonDecoder(mapper));
-                    c.defaultCodecs().maxInMemorySize(5*1024*1024);
+                    c.defaultCodecs().jackson2JsonDecoder(decoder);
+                    c.defaultCodecs().jackson2JsonEncoder(encoder);
+                    c.defaultCodecs().maxInMemorySize(5 * 1024 * 1024);
                 })
                 .build();
     }
--- InputService/src/main/java/vnedraid/inputservice/configs/JacksonConfig.java
+++ InputService/src/main/java/vnedraid/inputservice/configs/JacksonConfig.java
@@ -1,16 +1,17 @@
 package vnedraid.inputservice.configs;
 
 import com.fasterxml.jackson.core.json.JsonReadFeature;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
 
+    /** ObjectMapper, умеющий читать 0x1F */
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer allowCtrlChars() {
-        return builder -> builder.featuresToEnable(
-                JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature());
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true);
     }
 }
